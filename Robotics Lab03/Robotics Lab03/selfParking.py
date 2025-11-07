d2875d9ba84000f6cef454c9de75be0c07999a4a
tnunesueno <tnunesueno25@scienceleadership.org>
2025-11-06 21:23:16 -0500

    pushing ir angles and gloabls

--- Robotics Lab03/Robotics Lab03/selfParking.py
+++ Robotics Lab03/Robotics Lab03/selfParking.py
@@ -10,17 +10,30 @@ robot = Create3(Bluetooth("KIRITO-BOT"))  # Will connect to the first robot foun
 HAS_COLLIDED = False
 SENSOR2CHECK = 0
 ARRIVAL_THRESHOLD = 5
+STOP = False 
+SIDEWALL_FOUND = False 
+
+IR_ANGLES = [-65.3, -38.0, -20.0, -3.0, 14.25, 34.0, 65.3]
+
 
 # Implementation for fail-safe robots
 # EITHER BUTTON
 @event(robot.when_touched, [True, True])  # User buttons: [(.), (..)]
 async def when_either_button_touched(robot):
-    pass
+    global STOP
+    STOP = True
+    print("STOPPED")
+    await robot.set_wheel_speeds(0, 0)
+    await robot.set_lights_rgb(255, 0, 0)
 
 # EITHER BUMPER
 @event(robot.when_bumped, [True, True])  # [left, right]
 async def when_either_bumped(robot):
-    pass
+    global STOP
+    STOP = True
+    print("STOPPED")
+    await robot.set_wheel_speeds(0, 0)
+    await robot.set_lights_rgb(255, 0, 0)
 
 # ==========================================================
 
@@ -31,12 +44,15 @@ async def findWall():
     pass
 
 # === MOVE UNTIL WE FIND A GAP, TRACK LARGEST
-async def findGaps():
-    pass
+async def findGaps(robot):
+    #frontDist == 
+    
+    return largest_gap # ((x1, y1), (x2, y2))
 
 # === MOVE ALONG A FOUND GAP TO GET ITS START AND END
-async def calculateGap():
-    pass
+async def calculateGap(robot):
+
+    return gapPositions
 
 # === ADJUST COURSE TO ENSURE MOVING IN A STRAIGHT LINE
 async def courseCorrect():
